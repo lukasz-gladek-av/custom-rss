94a18e43ca20404ff835b8b521dd4c66e11de8b8
Łukasz Gładek <106964301+lukasz-gladek-av@users.noreply.github.com>
2024-05-13 19:34:43 +0200

    Update fetch-rss.js - article start with link fix

--- fetch-rss.js
+++ fetch-rss.js
@@ -75,7 +75,7 @@ async function fetchAndProcessFeed() {
           title: item.title,
           id: item.link,
           link: item.link,
-          content: itemArticleContent + '<br/>' + article.content,
+          content: itemArticleLink + '<br/>' + article.content,
           author: [{ name: item.author || 'Unknown' }],
         });
       } else {
